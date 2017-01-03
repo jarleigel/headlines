f01e0e51114bc7db9dd6c974d57aa4d2c7ccfa0e
Jarle Igeltjørn <jarle@gutua.com>
2017-01-03 20:17:26 +0100

    ferdig kap 2.3

--- headlines.py
+++ headlines.py
@@ -1,1 +1,10 @@
-start
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def get_news():
+  return "no news is good news"
+
+if __name__ == '__main__':
+  app.run(host='0.0.0.0', port=5000, debug=True)
